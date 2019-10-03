6d5e43a240fba6d14ed020ce30a3928d5868425a
ildar107 <ildar107wk@gmail.com>
2019-10-03 16:16:38 +0300

    feat:add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var result = []
+  var firstArr = first.split('').reverse();
+  var secondArr = second.split('').reverse();
+  var additional = 0;
+  for(let i = 0; i < firstArr.length; i++)
+  { 
+    if(additional > 0)
+      result.push(additional);
+    additional = 0;
+    for(let j = 0; j < secondArr.length; j++)
+    {
+      let multiplyValue = Number(firstArr[i]) * Number(secondArr[j]) + additional;
+      let newValue =  multiplyValue + (result[j+i] !== undefined ? result[j+i] : 0);
+      result[j+i] = newValue % 10;
+      additional = Math.floor(newValue / 10);
+    }
+  }
+    if(additional > 0)
+      result.push(additional);
+    return result.reverse().join(''); 
 }
