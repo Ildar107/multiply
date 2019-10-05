78df7b6844e1522ec73f5c589c929ec31def719b
Ildar107 <44859023+Ildar107@users.noreply.github.com>
2019-10-05 14:08:34 +0300

    refactor: change param name

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ module.exports = function multiply(first, second) {
   var result = []
   var firstArr = first.split('').reverse();
   var secondArr = second.split('').reverse();
-  var additional = 0;
+  var rest = 0;
   for(let i = 0; i < firstArr.length; i++)
   { 
-    if(additional > 0)
-      result.push(additional);
-    additional = 0;
+    if(rest > 0)
+      result.push(rest);
+    rest = 0;
     for(let j = 0; j < secondArr.length; j++)
     {
-      let multiplyValue = Number(firstArr[i]) * Number(secondArr[j]) + additional;
+      let multiplyValue = Number(firstArr[i]) * Number(secondArr[j]) + rest;
       let newValue =  multiplyValue + (result[j+i] !== undefined ? result[j+i] : 0);
       result[j+i] = newValue % 10;
-      additional = Math.floor(newValue / 10);
+      rest = Math.floor(newValue / 10);
     }
   }
-    if(additional > 0)
-      result.push(additional);
+    if(rest > 0)
+      result.push(rest);
     return result.reverse().join(''); 
 }
